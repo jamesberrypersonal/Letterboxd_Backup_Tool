7a53e7b129fd66e97f1991198a4d33e628e38e1c
jamesberrypersonal <james.berry.hameln@gmail.com>
2021-03-25 20:56:59 -0400

    Update letterboxd_spider.py
    Add collection of user like data for films
    Fix bug where spider would attempt to scrape like data as rating data if user liked film without rating it
    Add pagetest argument for spider for testing
    Add stub to sanitize user input

--- src/src/spiders/letterboxd_spider.py
+++ src/src/spiders/letterboxd_spider.py
@@ -11,36 +11,51 @@ class LetterboxdSpider(scrapy.Spider):
     name = "letterboxd"
     allowed_domains = ["letterboxd.com"]
 
-    # Constructor - optional argument to pass users letterboxd username (defaults to my own for now because I'm
-    # lazy and don't want to pass it in every time I test)
-    # TODO: clean default user arg
-    def __init__(self, user="fruityjames", **kwargs):
+    # Constructor
+    # Arguments:
+    #       -user: letterboxd account username to be crawled (Set to my own right now out bc I'm lazy while
+    #                testing)
+    #       -pagetest: flag to indicate only want to crawl the first page of the users films - intended to
+    #                    speed up testing when tests don't depend on crawling all pages (Set to true right now
+    #                    for same reason as above)
+    # TODO: fix issue of args not being set
+    # TODO: clean args before pushing to main
+    def __init__(self, user="fruityjames", pagetest=True, **kwargs):
         super().__init__(**kwargs)
-        self.user = user
+        self.page_test = pagetest
+        self.user = LetterboxdSpider.sanitize_user(user)
         self.start_urls = ["https://letterboxd.com/" + user + "/films/"]
-        # TODO: sanitize user input
 
     # Spiders parse function for retrieving and processing data - outputs dictionary of films, with title and
     # rating keys (rating set to n/a if film not rated by user)
     def parse(self, response):
 
         for film in response.css("li.poster-container"):
-            data = {}
-            data['Title'] = film.css("img::attr(alt)").get()
-            # Messy section here due to how ratings are displayed on page - numeric rating value only present
-            # at end of class attribute of a span element that may not exist (if user hasn't rated the film)
-            rating = film.css("p.poster-viewingdata.-rated-and-liked > span").xpath("@class").get()
-            if rating is not None:
-                # TODO: fix bug here stupid (ratings with half stars get processed as '1/2/10')
-                data['Rating'] = rating[-1] + "/10"
-            else:
-                data['Rating'] = "n/a"
+            data = {'Title': film.css("img::attr(alt)").get(), 'Rating': "n/a", 'Liked': "No"}
+            # Messy section here due to how ratings are displayed on page - user rating and liked status
+            # only present in a span element that may not exist (if user hasn't rated/liked the film) and
+            # rating numeric value only present within class attribute of the span element
+            rate = film.css("p.poster-viewingdata.-rated-and-liked > span")
+            if rate:
+                rated = rate.css('[class^="rating"]')
+                liked = rate.css('[class^="like"]')
+                if rated:
+                    data['Rating'] = rated.xpath("@class").get()[-1] + "/10"
+                if liked:
+                    data['Liked'] = "Yes"
             yield data
 
-        # Obtaining the next page (if it exists) to continue crawling
-        next_page = response.css("div.paginate-nextprev > a.next::attr(href)").get()
-        if next_page is not None:
-            url = response.urljoin(next_page)
-            # Sleep for 5sec here to limit crawling speed
-            time.sleep(5)
-            yield scrapy.Request(url, callback=self.parse)
+        # Obtaining the next page (if it exists) to continue crawling - if page_test is set then skip (to
+        # speed up testing that isn't specifically dependent crawling through all pages)
+        if not self.page_test:
+            next_page = response.css("div.paginate-nextprev > a.next::attr(href)").get()
+            if next_page is not None:
+                url = response.urljoin(next_page)
+                # Sleep for 5sec here to limit crawling speed
+                time.sleep(5)
+                yield scrapy.Request(url, callback=self.parse)
+
+    @staticmethod
+    # TODO: Implement input sanitizing
+    def sanitize_user(user):
+        return user
